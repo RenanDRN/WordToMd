274735085c23fda85a0e561069aa62d18038b9c9
RenanDRN <renan.reis.negrao@gmail.com>
2024-08-31 17:26:32 -0300

    feat: Novo design

--- main.py
+++ main.py
@@ -187,14 +187,6 @@ def iniciar_conversao():
  #   botao_converter.pack()
 #    raiz.mainloop()
 
-
-
-
-
-from PIL import Image
-import customtkinter
-import tkinter
-
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -213,36 +205,37 @@ class App(customtkinter.CTk):
         self.sidebar_frame.grid(row=0, column=0, rowspan=1, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
 
-        # Load and add the image above the text "CustomTkinter"
+        # Load and add the image above the text "WordToMd"
         imagem = Image.open("./img/novo-logo-itau-png-sem-fundo.png")  # Substitua pelo caminho da sua imagem
         imagem = imagem.resize((150, 150), Image.LANCZOS)  # Ajuste o tamanho conforme necessário
         self.logo = customtkinter.CTkImage(light_image=imagem, size=(150, 150))  # Usar CTkImage com tamanho especificado
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, image=self.logo, text="")  # Remover o texto
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
         
-        self.logo_label_text = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label_text = customtkinter.CTkLabel(self.sidebar_frame, text="WordToMd", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label_text.grid(row=1, column=0, padx=20, pady=(10, 10))
         
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Ler Arquivo", command=self.ler_arquivo)
         self.sidebar_button_1.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Sair", command=self.sair)
         self.sidebar_button_2.grid(row=3, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
-        self.sidebar_button_3.grid(row=4, column=0, padx=20, pady=10)
+        
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Aparência:", anchor="w")
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=4, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=5, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="Escala do aplicativo:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_label.grid(row=6, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%", "130%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 20))
 
         # create frame for header options
         self.header_frame = customtkinter.CTkFrame(self)
         self.header_frame.grid(row=0, column=1, padx=(20, 10), pady=(20, 20), sticky="nsew")
+        self.header_frame.grid_columnconfigure(0, weight=1)
+        self.header_frame.grid_rowconfigure(2, weight=1)
 
         # Add title label
         self.options_label = customtkinter.CTkLabel(self.header_frame, text="Selecione o tipo de cabeçalho:", font=customtkinter.CTkFont(size=16, weight="bold"))
@@ -254,23 +247,29 @@ class App(customtkinter.CTk):
         self.optionmenu_1.grid(row=1, column=0, padx=20, pady=(10, 10))
 
         # Add dynamic textbox with increased height
-        self.dynamic_textbox = customtkinter.CTkTextbox(self.header_frame, width=250, height=250)
+        self.dynamic_textbox = customtkinter.CTkTextbox(self.header_frame, state="disabled")
         self.dynamic_textbox.grid(row=2, column=0, padx=20, pady=(10, 10), sticky="nsew")
 
         # create frame for main textbox
         self.textbox_frame = customtkinter.CTkFrame(self)
         self.textbox_frame.grid(row=0, column=2, padx=(10, 20), pady=(20, 20), sticky="nsew")
+        self.textbox_frame.grid_columnconfigure(0, weight=1)
+        self.textbox_frame.grid_rowconfigure(0, weight=1)
 
         # create main textbox
-        self.textbox = customtkinter.CTkTextbox(self.textbox_frame, width=250)
+        self.textbox = customtkinter.CTkTextbox(self.textbox_frame)
         self.textbox.grid(row=0, column=0, padx=20, pady=(20, 20), sticky="nsew")
 
+        # Insert description text
+        self.textbox.insert("0.0", "Descrição:\n\n" + "Autor: Renan dos Reis Negrão\n\n" + "Aplicativo desenvolvido para facilitar a elaboração de novas documentaões técnicas para o GitHub Pages. Através da formatação de um arquivo word a aplicação reconhece e já converte o arquivo para um com extensão .md\n\n" + "Vamos de turma :)\n\n" +  "------------------------------\n\n" +  "Como usar o aplicativo?\n\n" +  "1. Selecione o tipo de cabeçalho desejado!\n\n" + "2. Clique em Ler Arquivo e selecione o arquivo word a ser convertido!\n"+ "OBS: O arquivo Word NÃO pode estar aberto durante a execução do programa!\n\n" + "3. Assim que selecionar o arquivo Word desejado, escolha qual o caminho onde será salvo seu arquivo .MD e a pasta com as imagens (Caso possua no documento)!\n\n")
+
+        # Disable the textbox
+        self.textbox.configure(state="disabled")
+
         # set default values
-        self.sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
         self.optionmenu_1.set("Topico")
-        self.textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
@@ -286,7 +285,14 @@ class App(customtkinter.CTk):
     def sidebar_button_event(self):
         print("sidebar_button click")
 
+    def ler_arquivo(self):
+        iniciar_conversao()
+
+    def sair(self):
+        self.quit()
+
     def update_textbox(self, value):
+        self.dynamic_textbox.configure(state="normal")
         self.dynamic_textbox.delete("1.0", "end")
         conteudo_markdown = []
         mes, dia, ano = "01", "01", "2023"  # Exemplo de data, você pode ajustar conforme necessário
@@ -311,6 +317,7 @@ class App(customtkinter.CTk):
             conteudo_markdown.append('---\n')
 
         self.dynamic_textbox.insert("1.0", "\n".join(conteudo_markdown))
+        self.dynamic_textbox.configure(state="disabled")
 
 if __name__ == "__main__":
     app = App()
