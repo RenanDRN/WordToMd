3fa13806c998e6379b6ca563eea0def6742e3522
Renan dos Reis Negrão <renan.reis.negrao@gmail.com>
2024-09-02 22:44:05 -0300

    fix: Formatacao Negrito e Italico

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import customtkinter
 from PIL import Image, ImageTk
 import sys
 
+# Função para obter o texto de um parágrafo do documento DOCX, mantendo o estilo de negrito e itálico quando aplicado.
 def obter_texto_paragrafo(para):
     """
     Função para obter o texto de um parágrafo do documento DOCX,
@@ -26,17 +27,27 @@ def obter_texto_paragrafo(para):
     """
     texto = ""
     if para.runs:
+        bold = False
+        italic = False
         for run in para.runs:
-            texto_run = run.text
             if run.bold:
-                texto_run = f"**{texto_run}**"
+                bold = True
             if run.italic:
-                texto_run = f"*{texto_run}*"
-            texto += texto_run
+                italic = True
+            texto += run.text
+        
+        if bold and italic:
+            texto = f"___{texto}___"
+        elif bold:
+            texto = f"**{texto}**"
+        elif italic:
+            texto = f"_{texto}_"
     else:
         texto = para.text
+    
     return texto
 
+# Função para salvar uma imagem em um diretório específico.
 def salvar_imagem(bytes_imagem, diretorio_imagem, nome_imagem, nome_arquivo):
     """
     Função para salvar uma imagem em um diretório específico.
@@ -57,6 +68,7 @@ def salvar_imagem(bytes_imagem, diretorio_imagem, nome_imagem, nome_arquivo):
         arquivo_imagem.write(bytes_imagem)
     return ".\\" + caminho_imagem
 
+# Função para converter um documento DOCX para Markdown.
 def converter_docx_para_markdown(caminho_docx, caminho_md_saida, diretorio_imagem, nome_arquivo, tipo_cabecalho):
     """
     Função para converter um documento DOCX para Markdown.
@@ -75,6 +87,7 @@ def converter_docx_para_markdown(caminho_docx, caminho_md_saida, diretorio_image
     mes = f'{data_atual.month:02d}'
     ano = f'{data_atual.year:04d}'
 
+    # Verifica o tipo de cabeçalho selecionado e adiciona ao conteúdo Markdown
     if(tipo_cabecalho == "Topico"):
         conteudo_markdown.append('---')
         conteudo_markdown.append('title: "Titulo da sua documentação"')
@@ -94,7 +107,7 @@ def converter_docx_para_markdown(caminho_docx, caminho_md_saida, diretorio_image
         conteudo_markdown.append('description: "Descrição da sua documentação"')
         conteudo_markdown.append('---\n')
 
-    # Definindo a função para adicionar parágrafos
+    # Função para adicionar parágrafos ao conteúdo Markdown
     def adicionar_paragrafo(paragrafo):
         texto = obter_texto_paragrafo(paragrafo)
         if paragrafo.style.name.startswith('Heading'):
@@ -105,7 +118,7 @@ def converter_docx_para_markdown(caminho_docx, caminho_md_saida, diretorio_image
         else:
             conteudo_markdown.append(f"{texto}\n")
 
-    # Definindo a função para adicionar tabelas
+    # Função para adicionar tabelas ao conteúdo Markdown
     def adicionar_tabela(tabela):
         conteudo_markdown.append("\n")
         # Extrair cabeçalhos da tabela
@@ -121,7 +134,7 @@ def converter_docx_para_markdown(caminho_docx, caminho_md_saida, diretorio_image
     contador_imagem = 1
     mapa_imagens = {}
 
-    # Processamento de imagens
+    # Processamento de imagens no documento
     for relacao in documento.part.rels.values():
         if "image" in relacao.reltype:
             parte_imagem = relacao._target
@@ -158,18 +171,23 @@ def converter_docx_para_markdown(caminho_docx, caminho_md_saida, diretorio_image
     with open(caminho_md_saida, "w", encoding="utf-8") as arquivo_md:
         arquivo_md.write("\n".join(conteudo_markdown))
 
+# Função para iniciar a conversão do arquivo DOCX para Markdown
 def iniciar_conversao(tipo_cabecalho):
     try:
+        # Abre um diálogo para selecionar o arquivo DOCX
         caminho_docx = filedialog.askopenfilename(filetypes=[("Arquivos Word", "*.docx")])
         nome_arquivo = os.path.splitext(os.path.basename(caminho_docx))[0]
+        # Abre um diálogo para selecionar o diretório de saída
         diretorio_saida = filedialog.askdirectory()
         caminho_md_saida = os.path.join(diretorio_saida, f"{nome_arquivo}.md")
         diretorio_imagem = os.path.join(diretorio_saida, f"img_{nome_arquivo}")
+        # Chama a função de conversão
         converter_docx_para_markdown(caminho_docx, caminho_md_saida, diretorio_imagem, nome_arquivo, tipo_cabecalho)
         messagebox.showinfo("Sucesso", "Conversão realizada com sucesso!")
     except Exception as e:
         messagebox.showerror("Erro", f"Ocorreu um erro: {str(e)}")
 
+# Função para obter o caminho absoluto para o recurso, funciona para dev e para PyInstaller
 def resource_path(relative_path):
     """ Obtenha o caminho absoluto para o recurso, funciona para dev e para PyInstaller """
     try:
@@ -180,25 +198,26 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
+# Classe principal da aplicação GUI
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        # configure window
+        # Configuração da janela principal
         self.title("WordToMd")
         self.geometry(f"{1100}x{580}")
 
-        # configure grid layout (2x1)
+        # Configuração do layout de grade (2x1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        # create sidebar frame with widgets
+        # Criação do frame da barra lateral com widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=1, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
 
-        # Load and add the image above the text "WordToMd"
+        # Carregar e adicionar a imagem acima do texto "WordToMd"
         imagem_path = resource_path("img/novo-logo-itau-png-sem-fundo.png")  # Use resource_path para obter o caminho correto
         imagem = Image.open(imagem_path)
         imagem = imagem.resize((150, 150), Image.LANCZOS)
@@ -225,13 +244,13 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 20))
 
-        # create frame for header options
+        # Criação do frame para opções de cabeçalho
         self.header_frame = customtkinter.CTkFrame(self)
         self.header_frame.grid(row=0, column=1, padx=(20, 10), pady=(20, 20), sticky="nsew")
         self.header_frame.grid_columnconfigure(0, weight=1)
         self.header_frame.grid_rowconfigure(2, weight=1)
 
-        # Add title label
+        # Adicionar rótulo de título
         self.options_label = customtkinter.CTkLabel(self.header_frame, text="Selecione o tipo de cabeçalho:", font=customtkinter.CTkFont(size=16, weight="bold"))
         self.options_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
@@ -240,51 +259,58 @@ class App(customtkinter.CTk):
                                                         command=self.update_textbox)
         self.optionmenu_1.grid(row=1, column=0, padx=20, pady=(10, 10))
 
-        # Add dynamic textbox with increased height
+        # Adicionar caixa de texto dinâmica com altura aumentada
         self.dynamic_textbox = customtkinter.CTkTextbox(self.header_frame, state="disabled")
         self.dynamic_textbox.grid(row=2, column=0, padx=20, pady=(10, 10), sticky="nsew")
 
-        # create frame for main textbox
+        # Criação do frame para a caixa de texto principal
         self.textbox_frame = customtkinter.CTkFrame(self)
         self.textbox_frame.grid(row=0, column=2, padx=(10, 20), pady=(20, 20), sticky="nsew")
         self.textbox_frame.grid_columnconfigure(0, weight=1)
         self.textbox_frame.grid_rowconfigure(0, weight=1)
 
-        # create main textbox
+        # Criação da caixa de texto principal
         self.textbox = customtkinter.CTkTextbox(self.textbox_frame)
         self.textbox.grid(row=0, column=0, padx=20, pady=(20, 20), sticky="nsew")
 
-        # Insert description text
+        # Inserir texto de descrição
         self.textbox.insert("0.0", "Descrição:\n\n" + "Autor: Renan dos Reis Negrão\n\n" + "Aplicativo desenvolvido para facilitar a elaboração de novas documentaões técnicas para o GitHub Pages. Através da formatação de um arquivo word a aplicação reconhece e já converte o arquivo para um com extensão .md\n\n" + "Vamos de turma :)\n\n" +  "------------------------------\n\n" +  "Como usar o aplicativo?\n\n" +  "1. Selecione o tipo de cabeçalho desejado!\n\n" + "2. Clique em Ler Arquivo e selecione o arquivo word a ser convertido!\n"+ "OBS: O arquivo Word NÃO pode estar aberto durante a execução do programa!\n\n" + "3. Assim que selecionar o arquivo Word desejado, escolha qual o caminho onde será salvo seu arquivo .MD e a pasta com as imagens (Caso possua no documento)!\n\n")
 
-        # Disable the textbox
+        # Desabilitar a caixa de texto
         self.textbox.configure(state="disabled")
 
-        # set default values
+        # Definir valores padrão
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
 
+    # Evento para abrir um diálogo de entrada
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
         print("CTkInputDialog:", dialog.get_input())
 
+    # Evento para mudar o modo de aparência
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
+    # Evento para mudar a escala do aplicativo
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
 
+    # Evento para o botão da barra lateral
     def sidebar_button_event(self):
         print("sidebar_button click")
 
+    # Função para ler o arquivo e iniciar a conversão
     def ler_arquivo(self):
         tipo_cabecalho = self.optionmenu_1.get()
         iniciar_conversao(tipo_cabecalho)
 
+    # Função para sair do aplicativo
     def sair(self):
         self.quit()
 
+    # Função para atualizar a caixa de texto dinâmica com base no tipo de cabeçalho selecionado
     def update_textbox(self, value):
         self.dynamic_textbox.configure(state="normal")
         self.dynamic_textbox.delete("1.0", "end")
@@ -313,6 +339,7 @@ class App(customtkinter.CTk):
         self.dynamic_textbox.insert("1.0", "\n".join(conteudo_markdown))
         self.dynamic_textbox.configure(state="disabled")
 
+# Inicialização da aplicação
 if __name__ == "__main__":
     app = App()
     app.mainloop()
